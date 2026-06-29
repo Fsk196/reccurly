071c977197e78f29768360d07b0e96e74e8ea1af
fsk196 <faissk196@gmail.com>
2026-06-29 11:19:46 +0530

    feat: project setup, nativewind, routing, tab navigation

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { Text } from "react-native";
 import React from "react";
 import { SafeAreaView as RNSSafeAreaView } from "react-native-safe-area-context";
 import { styled } from "nativewind";
